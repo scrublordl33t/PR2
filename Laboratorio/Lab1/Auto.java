3bd2a67f624e623d18c8beb079aecba259ea27fa
Daniele <43616904+scrublordl33t@users.noreply.github.com>
2024-03-14 10:50:11 +0100

    Modificato nome variabili nei metodi Set

--- Laboratorio/Lab1/Auto.java
+++ Laboratorio/Lab1/Auto.java
@@ -14,29 +14,29 @@ class Auto{
 	public String getModello(){
 		return this.modello;
 	}
-	public void setModello(String mod){
-		this.modello = mod;
+	public void setModello(String newModello){
+		this.modello = newModello;
 	}
 	
 	public String getMarca(){
 		return this.marca;
 	}	
-	public void setMarca(String mar){
-		this.marca = mar;
+	public void setMarca(String newMarca){
+		this.marca = newMarca;
 	}
 	
 	public double getCostoAlGiorno(){
 		return this.costoAlGiorno;
 	}
-	public void setCostoAlGiorno(double cost){
-		this.costoAlGiorno = cost;
+	public void setCostoAlGiorno(double newCostoAlGiorno){
+		this.costoAlGiorno = newCostoAlGiorno;
 	}
 	
 	public boolean getDisponibilita(){
 		return this.disponibilita;
 	}
-	public void setDisponibilita(boolean disp){
-		this.disponibilita = disp;
+	public void setDisponibilita(boolean newDisponibilita){
+		this.disponibilita = newDisponibilita;
 	}
 	
 }
